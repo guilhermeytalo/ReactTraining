8bfd27a48cf07a257eefad92dd49f723e55b9828
Guilherme Ytalo <guilherme.gramosa@totvs.com.br>
2022-01-12 23:27:43 -0300

    day 3

--- JavaScript - Programando a orientado a objetos/Conta/Conta.js
+++ JavaScript - Programando a orientado a objetos/Conta/Conta.js
@@ -1,5 +1,10 @@
+// Classe abstrata
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta) {
+            throw new Error("Você não deveria instanciar um objeto do tipo conta Diretamente \n \n \n");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
@@ -19,9 +24,9 @@ export class Conta {
         return this._saldo;
     }
 
+    // Metodo abstrato
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);     
+        throw new Error("O método salvar da conta é abstrato \n \n");
     }
 
     _sacar(valor, taxa) {
