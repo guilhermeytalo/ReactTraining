c5f72e6d68c43efccc3401d7c07d648335bb352b
Guilherme Ytalo <guilherme.gramosa@totvs.com.br>
2022-01-15 16:35:53 -0300

    react starting

---
+++ React - Entendendo como a biblioteca funciona/reactstart/src/App.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <section>
+      <form>
+        <input type={"text"} placeholder="Título"/>
+        <textarea placeholder="Escreva a sua nota"></textarea>
+        <button>Criar Nota
+        </button>
+      </form>
+
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>título</h3>
+            </header>
+            <p>escreva a sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default App;
